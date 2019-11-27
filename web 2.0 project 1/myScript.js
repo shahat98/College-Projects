c00661133a4794e7df0f47c724cf23531f943519
shahat98 <piratepirate454@gmail.com>
2019-11-27 20:26:52 +0200

    edit

--- web 2.0 project 1/myScript.js
+++ web 2.0 project 1/myScript.js
@@ -1,21 +1,36 @@
-var num;
-var alpha;
+var num; //number of charcter
+var alpha; //choosed charcter
 var flag=0;
 var myStorage=window.localStorage;
-function submited()
+window.addEventListener("load",function(e) //store load events
 {
-    if (myStorage.getItem("submit")==null)
+    var last_load="";
+    if (myStorage.getItem("load")!=null)
     {
-        myStorage.setItem("submit",1);
+        last_load=myStorage.getItem("load")+" , ";
     }
-    else
-        myStorage.setItem("submit",parseInt(myStorage.getItem("submit"))+1);
+    myStorage.setItem("load",last_load+"load  the "+(e.target)+" in "+ new Date());
+});
+
+var subBut=document.getElementsByClassName("button")[0];
+subBut.addEventListener("click",function(e) //store submit events
+{
+    var last_submit="";
+    if (myStorage.getItem("submit")!=null)
+        last_submit=myStorage.getItem("submit")+" , ";
+    myStorage.setItem("submit",last_submit+"submit "+e.target+" in "+ new Date());
+});
+function submited() //genrate the new charcters
+{
+    //clear output Div if exist
     var outDivAlpha=document.getElementById(2);
     for (;outDivAlpha.childNodes.length>0;)
         outDivAlpha.removeChild(outDivAlpha.childNodes[0]);
+    // clear output img if exist
     var imgDiv=document.getElementById(3);
     for (;imgDiv.childNodes.length>0;)
         imgDiv.removeChild(imgDiv.childNodes[0]);
+
     var txt_num=document.getElementById(1);
     num=parseInt(txt_num.value);
     if (num>26||num<0)
@@ -27,33 +42,40 @@ function submited()
         for (var i=0;i<26;i++)
              chk.push(0);
         var lstAlpha=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+        // genrate random charcters
         for (var i=0;i<num;i++)
         {
             var rnd=random();
-            if (chk[rnd])
+            if (chk[rnd]) // check to avoid delicating character
             {
                 i--;
                 continue;
             }
             chk[rnd]=1;
+            // create a new li
             var newItem=document.createElement("li");
             var newAlpha=document.createTextNode(lstAlpha[rnd]);
-            if (lstAlpha[rnd]==undefined)
-                alert(rnd);
             newItem.appendChild(newAlpha);
-            var outAlpha=document.getElementsByTagName("li");
             newItem.addEventListener("click",function(e){
                 var imgDiv=document.getElementById(3);
+                // clear imgDiv
                 for (;imgDiv.childNodes.length>0;)
                     imgDiv.removeChild(imgDiv.childNodes[0]);
+
                 alpha=this.textContent;
                 flag=1;
+                //store click charcter events
+                var last_click="";
+                if (myStorage.getItem("click character")!=null)
+                    last_click=myStorage.getItem("click character")+" , ";
+                myStorage.setItem("click character",last_click+"click character "+this.textContent+" in "+ new Date());
                }); 
+
             outDivAlpha.appendChild(newItem);
         }
     }
 }
-function random()
+function random() // genrate a random number
 {
     var ret=Math.random();
 		ret*=1000;
@@ -61,7 +83,7 @@ function random()
         ret%=26;
 		return ret;
 }
-function play()
+function play() // show img of selected charcter
 {
     if (!flag)
         return;
